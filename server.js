91bc9f1133b78a8ca4db686a070f1e56fd2e94ce
‘Michael <michael.daniels.jr@gmail.com>
2018-02-28 18:10:34 -0700

    working prototype

--- server.js
+++ server.js
@@ -52,6 +52,24 @@ app.get('/upvote/:id', (req, res)=>{
   })
 })
 
+app.get('/downvote/:id', (req, res)=>{
+  knex('movies').where('id', req.params.id)
+  .then((results)=>{
+    console.log('FIRST RESULT: ',results);
+
+    let result = results[0].votes
+    console.log('FIRST RESULT: ',result);
+    knex('movies').where('id', req.params.id)
+    .update({votes: result-1}).then(()=>{
+      res.redirect('/movies')
+      console.log('RESULT: ',result)
+
+    })
+    //console.log('RESULT: ',result)
+    //res.redirect('/movies')
+  })
+})
+
 
 app.get('/students/:id', (req, res) => {
   knex('students').where('class_id', req.params.id).then((result) => {
